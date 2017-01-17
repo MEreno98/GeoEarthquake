819e72b16896ff1fdbc7857c8e84199958b07bd0
Jon Sanchez <jonsanchez19@gmail.com>
2017-01-17 21:09:25 +0100

    Importado GoogleMaps al MainStoryBoard. La aplicación arranca ya con un mapa en un punto  y un marker.

--- GeoEarthquake/ViewController.swift
+++ GeoEarthquake/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        
+        mapView.isMyLocationEnabled = true
+        
+        self.view = mapView
+        
+        let marker = GMSMarker()
+        
+        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+        
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+        
     }
 
     override func didReceiveMemoryWarning() {
