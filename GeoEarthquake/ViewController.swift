00970f15334844474ee73427e164ec0378b7464c
Mikel Ereño <MEreno1995@hotmail.com>
2017-01-17 19:49:32 +0100

    Initial Commit

---
+++ GeoEarthquake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeoEarthquake
+//
+//  Created by Mikel Ereño on 17/1/17.
+//  Copyright © 2017 8FPROGMM11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
