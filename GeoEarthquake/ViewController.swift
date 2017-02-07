48854ef3f2cbb4fe759aa35937367b7ecac2e621
Jon Sanchez <jonsanchez19@gmail.com>
2017-02-07 19:36:37 +0100

    Petición datos al servidor.

--- GeoEarthquake/ViewController.swift
+++ GeoEarthquake/ViewController.swift
@@ -8,12 +8,16 @@
 
 import UIKit
 import GoogleMaps
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        relizarBusqueda()
         
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
@@ -36,7 +40,27 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func relizarBusqueda(){
+        Alamofire.request("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson").responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print(json)
+                
+            self.generar_marcas(informacion: json)
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    func generar_marcas(informacion info:JSON ){
+       
+        
+        
+        
+    }
 
 }
 
