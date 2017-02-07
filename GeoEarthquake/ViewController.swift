3e06899565158c838038b978bb7fa438abd6867d
Jon Sanchez <jonsanchez19@gmail.com>
2017-02-07 20:29:04 +0100

    Creado la vista Global

--- GeoEarthquake/ViewController.swift
+++ GeoEarthquake/ViewController.swift
@@ -13,30 +13,39 @@ import SwiftyJSON
 
 class ViewController: UIViewController, GMSMapViewDelegate {
 
+    
+    var mapView : GMSMapView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //Generamos el punto desde donde queremos empezar a visualizar el mapa.
+        let camera = GMSCameraPosition.camera(withLatitude: 35.895, longitude: -77.036, zoom: 4)
+        
+        //Generamos el mapa con la camara indicaca.
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
 
+        //Propiedades del mapa.
+        
+        if let mapa = mapView{
+            mapa.isMyLocationEnabled = true
+            mapa.mapType = kGMSTypeTerrain
+            mapa.delegate = self
+            
+            self.view = mapa
+        }
+        
+        //Procedemos a buscar los terremetos que han ocurrido.
         relizarBusqueda()
         
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6)
-
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        //let marker = GMSMarker()
         
-        mapView.isMyLocationEnabled = true
-        mapView.mapType = kGMSTypeTerrain
-        mapView.delegate = self
+        //marker.position = CLLocationCoordinate2DMake(35.895, -77.036)
         
-        self.view = mapView
-        
-        let marker = GMSMarker()
-        
-        marker.position = CLLocationCoordinate2DMake(35.895, -77.036)
-        
-        marker.title = "Estados Unidos"
-        marker.snippet = "Washington D. C."
-        marker.map = mapView
+        //marker.title = "Estados Unidos"
+        //marker.snippet = "Washington D. C."
+        //marker.map = mapView
         
     }
 
@@ -55,10 +64,10 @@ class ViewController: UIViewController, GMSMapViewDelegate {
         Alamofire.request("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson").responseJSON { response in
             switch response.result {
             case .success(let value):
-                let json = JSON(value)
-                print(json)
-                
-            self.generar_marcas(informacion: json)
+                //La petición me devuelve un JSON, el cual guardo para su posterior procesamiento.
+                let info_terremetos = JSON(value)
+                //Generar_marcas es el encargado de colocar los puntos en nuestro mapa.
+                self.generar_marcas(informacion: info_terremetos)
                 
             case .failure(let error):
                 print(error)
@@ -67,6 +76,9 @@ class ViewController: UIViewController, GMSMapViewDelegate {
     }
     func generar_marcas(informacion info:JSON ){
        
+        for terremeto in info{
+            print(terremeto)
+        }
     }
 
 }
